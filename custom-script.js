a1b894e5f0fe58a15dc2aed4a5953d9ba6541ecd
Yewon Seo <syw1236@naver.com>
2023-10-08 18:10:28 +0900

    Modify button listener

--- custom-script.js
+++ custom-script.js
@@ -1,20 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // 버튼 클릭 시 알림 메시지 표시
-    const alertButton = document.getElementById("alert-button");
-    alertButton.addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  // 버튼 클릭 시 알림 메시지 표시
+  const alertButton = document.getElementById("alert-button");
+  /*     alertButton.addEventListener("click", function() {
         alert("Button clicked!");
-    });
+    }); */
+  alertButton.addEventListener("click", function () {
+    window.location.href = "learn-more.html";
+  });
 
-    // 이미지 변경
-    const imageElement = document.getElementById("change-image");
-    const originalImageSrc = imageElement.src;
-    const newImageSrc = "./images/mountain.jpg";
+  // 이미지 변경
+  const imageElement = document.getElementById("change-image");
+  const originalImageSrc = imageElement.src;
+  const newImageSrc = "./images/mountain.jpg";
 
-    imageElement.addEventListener("mouseover", function() {
-        imageElement.src = newImageSrc;
-    });
+  imageElement.addEventListener("mouseover", function () {
+    imageElement.src = newImageSrc;
+  });
 
-    imageElement.addEventListener("mouseout", function() {
-        imageElement.src = originalImageSrc;
-    });
+  imageElement.addEventListener("mouseout", function () {
+    imageElement.src = originalImageSrc;
+  });
 });
